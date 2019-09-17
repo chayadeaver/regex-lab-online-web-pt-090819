d43482de45b74423d90791e171ddbd538ad55862
Chaya Deaver <chayadeaver@gmail.com>
2019-09-17 17:12:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,9 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z]\./)
+  text.match(/^[A-Z].*[a-zA-z][.]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.match
 end
